423be46caca378dd98e031e370af1c89c93b32fc
swayinx <capazape@unsa.edu.pe>
2026-06-01 20:14:05 -0500

    fix: adaptar a CSFML 3 (Arch Linux) - includes, sfTrue, sfVideoMode, sfRenderWindow_create

--- turtlec.h
+++ turtlec.h
@@ -1,7 +1,7 @@
 #ifndef TURTLEC_H
 #define TURTLEC_H
 
-#include <SFML/Graphics.h>
+#include <CSFML/Graphics.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -11,50 +11,50 @@ extern "C" {
 
 typedef struct Turtle Turtle;
 typedef struct TurtleApp TurtleApp;
-typedef void (*TurtleDrawFunc)(Turtle *turtle);
+typedef void (*TurtleDrawFunc)(Turtle* turtle);
 
 struct Turtle {
-  sfRenderWindow *window;
-  sfVertexArray *lines;
-  sfConvexShape *shape;
-  sfVector2f pos;
-  float heading;
-  bool penIsDown;
-  sfColor penColor;
-  float speed;
-  unsigned int width;
-  unsigned int height;
+    sfRenderWindow* window;
+    sfVertexArray* lines;
+    sfConvexShape* shape;
+    sfVector2f pos;
+    float heading;
+    bool penIsDown;
+    sfColor penColor;
+    float speed;
+    unsigned int width;
+    unsigned int height;
 };
 
 struct TurtleApp {
-  sfRenderWindow *window;
-  Turtle turtle;
+    sfRenderWindow* window;
+    Turtle turtle;
 };
 
-void turtleInit(Turtle *turtle, sfRenderWindow *window, unsigned int width, unsigned int height);
-void turtleDestroy(Turtle *turtle);
+void turtleInit(Turtle* turtle, sfRenderWindow* window, unsigned int width, unsigned int height);
+void turtleDestroy(Turtle* turtle);
 
-void turtleForward(Turtle *turtle, float dist);
-void turtleBackward(Turtle *turtle, float dist);
-void turtleRight(Turtle *turtle, float angle);
-void turtleLeft(Turtle *turtle, float angle);
-void turtlePenUp(Turtle *turtle);
-void turtlePenDown(Turtle *turtle);
-void turtleSetColor(Turtle *turtle, uint8_t r, uint8_t g, uint8_t b);
-void turtleGoTo(Turtle *turtle, float x, float y);
-void turtleSetSpeed(Turtle *turtle, float speed);
-void turtleCircle(Turtle *turtle, float radius);
-void turtleClear(Turtle *turtle);
-void turtleHome(Turtle *turtle);
-void turtleDraw(Turtle *turtle);
-void turtleUpdateDisplay(Turtle *turtle);
+void turtleForward(Turtle* turtle, float dist);
+void turtleBackward(Turtle* turtle, float dist);
+void turtleRight(Turtle* turtle, float angle);
+void turtleLeft(Turtle* turtle, float angle);
+void turtlePenUp(Turtle* turtle);
+void turtlePenDown(Turtle* turtle);
+void turtleSetColor(Turtle* turtle, uint8_t r, uint8_t g, uint8_t b);
+void turtleGoTo(Turtle* turtle, float x, float y);
+void turtleSetSpeed(Turtle* turtle, float speed);
+void turtleCircle(Turtle* turtle, float radius);
+void turtleClear(Turtle* turtle);
+void turtleHome(Turtle* turtle);
+void turtleDraw(Turtle* turtle);
+void turtleUpdateDisplay(Turtle* turtle);
 
-TurtleApp *turtleAppCreate(unsigned int width, unsigned int height, const char *title);
-Turtle *turtleAppGetTurtle(TurtleApp *app);
-void turtleAppRun(TurtleApp *app);
-void turtleAppDestroy(TurtleApp *app);
+TurtleApp* turtleAppCreate(unsigned int width, unsigned int height, const char* title);
+Turtle* turtleAppGetTurtle(TurtleApp* app);
+void turtleAppRun(TurtleApp* app);
+void turtleAppDestroy(TurtleApp* app);
 
-void turtleRun(TurtleDrawFunc drawFunc, unsigned int width, unsigned int height, const char *title);
+void turtleRun(TurtleDrawFunc drawFunc, unsigned int width, unsigned int height, const char* title);
 
 #ifdef __cplusplus
 }
