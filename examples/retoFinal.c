e75f413066d2da68840663b155b22920ecb2fe30
swayinx <capazape@unsa.edu.pe>
2026-06-02 00:42:41 -0500

    añadí el archivo del reto final, elegí modificar el arbol fractal para que tenga 3 ramas, derecha, medio e izquierda

---
+++ examples/retoFinal.c
@@ -0,0 +1,44 @@
+#include "../turtlec.h"
+
+void branch(Turtle* t, float length, int deep, float angle) {
+    if (deep == 0) return;
+
+    turtleForward(t, length);
+
+    turtleLeft(t, angle);
+    branch(t, length * 0.7, deep - 1, angle);
+
+    turtleRight(t, angle);
+    branch(t, length * 0.7, deep - 1, angle);
+
+    turtleRight(t, angle);
+    branch(t, length * 0.7, deep - 1, angle);
+
+    turtleLeft(t, angle);
+
+    if (deep <= 3) {
+        turtleSetColor(t, 32, 171, 17);
+    } else {
+        turtleSetColor(t, 71, 44, 44);
+    }
+    turtleBackward(t, length);
+}
+
+int main(void) {
+    TurtleApp* app = turtleAppCreate(800, 800, "arbol");
+    if (app == NULL) return 1;
+    Turtle* t = turtleAppGetTurtle(app);
+
+    turtleSetSpeed(t, 15);
+    turtleLeft(t, 90);
+    turtlePenUp(t);
+    turtleBackward(t, 200.0);
+    turtlePenDown(t);
+
+    branch(t, 150.0, 5, 30.0);
+
+    turtleAppRun(app);
+    turtleAppDestroy(app);
+
+    return 0;
+}
